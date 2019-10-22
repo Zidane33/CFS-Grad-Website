f9d893498a208128099fb44e5f8e0cf57c8d48d5
Zidane <mohamedzidan@mohameds-MacBook-Air.local>
2019-10-22 00:21:13 -0400

    gulp compiling sass now

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
 });
+
+//Default task
+
+gulp.task("default", gulp.series("sass"));
